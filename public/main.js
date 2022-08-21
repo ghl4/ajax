b48cc613ad33825fcd70334bab0683ebd3fd2d88
guo <1799387597@qq.com>
2022-08-21 12:54:05 +0800

    提交 ajax

---
+++ public/main.js
@@ -0,0 +1,106 @@
+let n = 1;
+
+getPage.onclick = ()=>{
+    const request = new XMLHttpRequest()
+    request.open('GET',`/page${n+1}`);
+    request.onreadystatechange=()=>{
+        if(request.readyState===4 && request.status===200 && n<3){
+            console.log(request.response)
+            const array = JSON.parse(request.response)
+            array.forEach(item => {
+                const li = document.createElement('li')
+                li.textContent = item.id
+                xxx.appendChild(li)
+            }); 
+            n+=1;
+           console.log(n);
+        }
+    }
+    request.send()
+}
+
+getJSON.onclick = ()=>{
+    const request = new XMLHttpRequest()
+    request.open('GET','/5.json')
+    request.onreadystatechange=()=>{
+        if(request.readyState===4 && request.status===200){
+            console.log(request.response)
+            const object = JSON.parse(request.response)
+            myname.textContent=object.name
+            console.log(object)
+        }
+    }
+    request.send()
+}
+
+getXML.onclick=()=>{
+    const request = new XMLHttpRequest()
+    request.open('GET','/4.xml')
+    request.onreadystatechange=()=>{
+        if(request.readyState===4 && request.status==200){
+            const dom = request.responseXML;
+            const text = dom.getElementsByTagName('warning')[0].textContent
+            console.log(text.trim())
+        }
+    }
+    request.send()
+}
+
+getHTML.onclick=()=>{
+    const request = new XMLHttpRequest()
+    request.open('GET','/3.html')
+    request.onload=()=>{
+        const div = document.createElement('div')
+        div.innerHTML = request.response
+        document.body.appendChild(div)
+    }
+    request.onerror=()=>{ }
+    request.send()
+}
+
+getJS.onclick = ()=>{
+    const request = new XMLHttpRequest()
+    request.open('GET','/2.js');
+    request.onload = ()=>{  
+    const script = document.createElement('script')
+    script.innerHTML = request.response
+    document.head.appendChild(script) 
+    }
+    request.onerror = ()=>{
+    console.log("失败")
+    }
+    request.send()
+}
+
+// getCSS.onclick = ()=>{
+//     const request = new XMLHttpRequest()
+//     request.open('GET','/style.css');
+//     request.onload = ()=>{
+//     console.log(request.response)
+//     const style = document.createElement('style')
+//     style.innerHTML = request.response
+//     document.head.appendChild(style)
+    
+//     }
+//     request.onerror = ()=>{
+//     console.log("失败")
+//     }
+//     request.send()
+// }
+
+getCSS.onclick = ()=>{
+    const request = new XMLHttpRequest()
+    request.open('GET','/style.css');
+    request.onreadystatechange = ()=>{
+        if(request.readyState===4){
+            if(request.status >= 200 && request.status<300){
+                const style = document.createElement('style')
+                style.innerHTML = request.response
+                document.head.appendChild(style)
+            }else{
+                alert('失败')
+            }
+        }
+    }
+    request.send()
+}
